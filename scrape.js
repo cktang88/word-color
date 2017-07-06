0f241dc0ce1b32471325bb31d140f07278ef0d3b
GreenLantern101 <arctangent20@yahoo.com>
2017-07-06 15:54:11 -0500

    Fix pic url extraction

--- scrape.js
+++ scrape.js
@@ -1,44 +1,34 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const Promise = require('bluebird')
+const Promise = require('bluebird');
 /**
  * Get the image src for all links, options.keyword is required.
  */
 const getImages = (searchword) => {
   // sample clean url: https://www.bing.com/images/search?q=dogs&view=detailv2&selectedindex=5
   const baseurl = 'https://www.bing.com/images/search?q=%&view=detailv2';
-  const url = `${baseurl.replace('%', encodeURIComponent(searchword))}&selectedindex=0`;
-
-  const options = {
-    uri: url,
-    headers: {
-      // spoof user-agent
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-    }
-  };
-
-  // console.log(url);
-  return extractPicUrls(options);
+  // NOTE: DO NOT use headers --> spoofing headers makes extracting pics go haywire
+  const url = `${baseurl.replace('%', encodeURIComponent(searchword))}&selectedindex=0`
+  return extractPicUrls(url);
 };
 
-// extract all pic urls
-const extractPicUrls = options => rp(options)
-  .then(body => {
-    console.log('finding image urls...');
+// private helper method
+// extract all pic urls from url
+const extractPicUrls = url => rp(url)
+  .then((body) => {
+    // console.log('finding image urls...');
     const $ = cheerio.load(body);
     // array of images returned
-    console.log($('img'))
-    return $('img');
+    return $('img').toArray();
   })
-  .map((img) => {
-    const link = $(img).attr('src');
+  .map((elem) => {
+    const link = elem.attribs.src;
     return (link.substr(0, 4) === 'http') ? link : undefined;
   })
-  .filter(url => url !== undefined)
-  .catch(err => {
+  .filter((url) =>  url !== undefined)
+  .catch((err) => {
     console.log(err);
   });
 
 // TODO: simplify
-const Scraper = module.exports;
-Scraper.getImages = getImages;
+module.exports = getImages;
